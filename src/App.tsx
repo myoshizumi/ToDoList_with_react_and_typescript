2387094e70a9cb09aa86c7f41626d1abbf4e70f9
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-06-19 14:02:53 +0900

    Add form with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import './App.css';
+import NewTodo from './components/NewTodo';
+import Todos from './components/Todos';
+import TodosContextProvider from './store/todos-context';
 
 function App() {
+ 
   return (
     <div>
+      <TodosContextProvider>
+        <NewTodo />
+        <Todos />
+      </TodosContextProvider>
     </div>
   );
 }
